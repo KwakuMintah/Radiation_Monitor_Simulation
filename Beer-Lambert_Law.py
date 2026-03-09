2b0e94dc027720574ace226036ba0f5af90fd9ab
Kwaku Mintah <125976893+KwakuMintah@users.noreply.github.com>
2026-03-09 12:01:20 +0000

    Update Beer-Lambert_Law.py

--- Beer-Lambert_Law.py
+++ Beer-Lambert_Law.py
@@ -10,7 +10,7 @@ materials = {
     "Aluminium" : {"density": 2.7, "mu": 0.136},
     "Steel": {"density": 7.85, "mu": 0.15},
     "Concrete": {"density": 2.4, "mu": 0.03},
-    "Tungsten": {"denisty": 19.254, "mu": 0.5} #I'm not sure where Vasan
+    "Tungsten": {"density": 19.254, "mu": 0.5} #I'm not sure where Vasan
     #got their values for mu from so I have given tungsten the same value as 
     #lead until I find it
 }
@@ -25,7 +25,6 @@ class ShieldingObject:
         if mat is None:
             mat = materials["Lead"]
         self.params = kwargs
-        self.n = self.params.get("num_particles")
     #Replicating Josh's code for defining the volume of the shield. The object will always be a block in this case
     def volume(self):
         self.l = self.params.get("length")
@@ -35,47 +34,49 @@ class ShieldingObject:
             raise ValueError("Block requires 'length', 'width', and 'depth'")
         return self.l * self.w * self.h 
 
-    def particle_sim(self):
-        self.particles = np.zeros((self.n, 3))
-        self.particles[:,0] = np.random.uniform(0, self.w, size=self.n)
-        self.particles[:,1] = np.full(self.n,0)
 
-        particle_states = np.zeros(self.n,dtype=int) #0->active,1->absorbed,2->transmitted
-        speed = 100
-        self.particles[:, 2] = np.full(self.n,speed)
+def particle_sim(shield,num_particles):
+    particles = np.zeros((num_particles,3))
+    particles[:,0] = np.random.uniform(0, shield.w, size=num_particles)
+    particles[:,1] = np.full(num_particles,0)
 
-        angles = np.random.uniform(np.pi/3,2*np.pi/3,size=self.n)
-        self.particles[:,0] += np.cos(angles) * self.particles[:,2]
-        self.particles[:,1] += np.sin(angles) * self.particles[:,2]
+    particle_states = np.zeros(num_particles,dtype=int) #0->active,1->absorbed,2->transmitted
+    speed = 100
+    particles[:, 2] = np.full(num_particles,speed)
 
-        detection_layer_y = (self.h // 2) + self.l
-        transmission_probabilities = np.exp(-self.mu * self.l)
-        #results[material] = np.sum(particle_states == 2)
+    angles = np.random.uniform(np.pi/3,2*np.pi/3,size=num_particles)
+    particles[:,0] += np.cos(angles) * particles[:,2]
+    particles[:,1] += np.sin(angles) * particles[:,2]
+
+    detection_layer_y = (shield.h // 2) + shield.l
+    transmission_probabilities = np.exp(-shield.mu * shield.l)
+    #results[material] = np.sum(particle_states == 2)
         
-        def update(self):
-            nonlocal particle_states
+    def update():
+        nonlocal particle_states, particles
 
-            active_particles = particle_states == 0
-            self.particles[active_particles, 0] += np.cos(angles[active_particles]) * 1
-            self.particles[active_particles, 1] += np.sin(angles[active_particles]) * 1
+        active_particles = particle_states == 0
+        particles[active_particles, 0] += np.cos(angles[active_particles]) * 1
+        particles[active_particles, 1] += np.sin(angles[active_particles]) * 1
 
-            for i in range(self.n):
-                if particle_states[i] == 0:
-                    if self.h // 2 <= self.particles[i,1] <= (self.h // 2 + self.l):
-                        if np.random.rand() < transmission_probabilities:
-                            particle_states[i] = 2 #Transmitted
-                        else:
-                            particle_states[i] = 1 #Absorbed
-            if np.all(particle_states != 0):
-                print("Simulation Ended")
-        results[material] = np.sum(particle_states == 2)
+        for i in range(num_particles):
+            if particle_states[i] == 0:
+                if shield.h // 2 <= particles[i,1] <= (shield.h // 2 + shield.l):
+                    if np.random.rand() < transmission_probabilities:
+                        particle_states[i] = 2 #Transmitted
+                    else:
+                        particle_states[i] = 1 #Absorbed
+        if np.all(particle_states != 0):
+            print("Simulation Ended")
+    results[material] = np.sum(particle_states == 2)
 
 
-shield_test = ShieldingObject("Lead",length=10,width=15,depth=50,num_particles=1000)
-vol_test = shield_test.volume()
+
 
 for material in materials.keys():
-    shield_test.particle_sim()
+    shield_test = ShieldingObject(material,length=10,width=15,depth=50)
+    vol_test = shield_test.volume()
+    particle_sim(shield_test,num_particles=1000000)
 
 materials_list = list(materials.keys())
 transmitted_counts = [results[material] for material in materials_list]
